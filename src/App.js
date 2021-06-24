444306b8542d97305850694d495bd6feb6b1c8c6
borjankorunoski <korunoskiborjan@gmail.com>
2021-06-24 21:19:08 +0200

    [MC-2] Added authentication page with register and login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import HomePage from "./components/pages/HomePage";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from "react-bootstrap";
+import AuthenticationPage from "./components/pages/AuthenticationPage";
 const App = () => {
   return (
     <Container>
       <HomePage/>
+      <hr/>
+      <AuthenticationPage/>
     </Container>
   );
 }
