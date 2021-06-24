8ba1b1cc592461685a0c3c380038826d890b0083
DimovNikola <nikoladimov45@yahoo.com>
2021-06-24 20:29:44 +0200

    file with locations created

---
+++ src/Data/LocationsData.js
@@ -0,0 +1,27 @@
+export const locations = [
+    {
+        "title": "ФЕИТ",
+        "working-hours": "08:00 - 20:00",
+        "address": "Ругер Бошковиќ, Скопје 1000",
+    },
+    {
+        "title": "Public Room",
+        "working-hours": "08:00 - 20:00",
+        "address": "MK, 50-та Дивизија 22, Скопје 1000",
+    },
+    {
+        "title": "1000 книги",
+        "working-hours": "08:00 - 20:00",
+        "address": "ГТЦ кат 2",
+    },
+    {
+        "title": "Другарче",
+        "working-hours": "08:00 - 20:00",
+        "address": "Никола Русински 1",
+    },
+    {
+        "title": "Браќа Миладиновци",
+        "working-hours": "08:00 - 20:00",
+        "address": "Булевар Партизански одреди бр.22 Скопје",
+    }
+]
